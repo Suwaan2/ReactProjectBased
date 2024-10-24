e1c328f7fd8803a10b690eca276ac95199cc3f30
Suan KC <kcsuan424@gmail.com>
2024-10-24 20:52:28 +0545

    day 2(Hooks&Negative)

--- reactprojectState/src/App.jsx
+++ reactprojectState/src/App.jsx
@@ -2,25 +2,33 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [Counter, setCounter] = useState(90);
+  const [Counter, setCounter] = useState(9);
 
   // let Counter;
 
   const addvalue = () => {
     //  return  Counter = Counter + 5
     setCounter(Counter + 1);
+    if (Counter < 0) {
+      console.log("Number is Negative", Math.random());
+    } else {
     console.log("value added", Counter);
+    }
   };
 
   const removeValue = () => {
     setCounter(Counter - 1);
-    console.log("Value removed", Counter);
+    if (Counter < 0) {
+      console.log("Number is Negative", Math.random());
+    } else {
+      console.log("Value removed", Counter);
+    }
   };
 
   return (
     <>
       <h1>Hello State</h1>
-      <h2>Counter Value: {Counter}</h2>
+      <h2>Counter Value: {Counter} </h2>
       <button onClick={addvalue}>Add Value</button>
       <button onClick={removeValue}>Remove Value</button>
       <p>Real time Value : {Counter}</p>
