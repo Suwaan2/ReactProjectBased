74ffdfbe9dd85b60a655a225a940ab42ec5ae2db
Suan KC <kcsuan424@gmail.com>
2024-11-07 19:54:07 +0545

    Fifth day(After long Break )

--- customhook/src/App.jsx
+++ customhook/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
 
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () =>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
 
   return (
    <>
--- customhook/src/components/index.js
+++ customhook/src/components/index.js
@@ -2,3 +2,9 @@ import InputBox from "./InputBox";
 
 
 export {InputBox}
+
+
+
+
+
+
