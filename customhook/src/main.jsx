4d61e3b132346a68cd11ad65f3fca1a4553c53c0
Suan KC <kcsuan424@gmail.com>
2024-11-08 19:47:01 +0545

    Error While Executing(Looking for Fix)

--- customhook/src/main.jsx
+++ customhook/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import InputBox from './components/InputBox.jsx'
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <InputBox />
+  
 
   </StrictMode>,
 )
