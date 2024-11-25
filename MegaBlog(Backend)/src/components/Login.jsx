0e1f4f064314667f8fbbb9a23d05b03ab501b94d
Suan KC <kcsuan424@gmail.com>
2024-11-25 20:16:10 +0545

    some additional components

--- MegaBlog(Backend)/src/components/Login.jsx
+++ MegaBlog(Backend)/src/components/Login.jsx
@@ -6,10 +6,11 @@ import { useDispatch } from "react-redux";
 import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 
+
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useState("");
+  const { register, handleSubmit } = useForm();
   const [error, setError] = useState("");
 
   const login = async (data) => {
