d52c39b83bbad59b539f26ef6da29c3dcd7bd4f1
Suan KC <kcsuan424@gmail.com>
2024-11-28 13:37:59 +0545

    Debugging Remaining

--- MegaBlog(Backend)/src/pages/AllPost.jsx
+++ MegaBlog(Backend)/src/pages/AllPost.jsx
@@ -3,10 +3,25 @@ import appwriteService from '../appwrite/config'
 import { Container, PostCard } from '../components'
 
 function AllPost() {
-   
+   const [posts, setPosts] = useState([])
+   useEffect(()=>{},[])
+   appwriteService.getPosts([]).then((posts) => {
+    if(posts){
+      setPosts(posts.documents)
+    }
+   })
+  
   return (
-    <div>
-      
+    <div className='w-full py-8'> 
+    <Container>
+      <div className='flex flex-wrap'>
+        {posts.map((post)=>(
+          <div key={post.$id} className='p-2 w-1/2'>
+            <PostCard post={post}/>
+          </div>
+        ))}
+      </div>
+        </Container>
     </div>
   )
 }
