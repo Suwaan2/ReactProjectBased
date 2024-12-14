e78f309c4fd3004baac8ebb3590c8571f4c78bc5
Suan KC <kcsuan424@gmail.com>
2024-12-14 21:16:51 +0545

    Tinymice api key needed

--- MegaBlog(Backend)/src/appwrite/auth.js
+++ MegaBlog(Backend)/src/appwrite/auth.js
@@ -31,7 +31,7 @@ export class AuthService {
 
     async login({email, password}){
         try {
-            await this.account.createEmailSession(email, password)
+            await this.account.createEmailPasswordSession(email, password)
         } catch (error) {
             throw error;
         }
