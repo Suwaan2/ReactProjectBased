9ab927bce4faec40e57176a50d129e60c9f25c12
Suan KC <kcsuan424@gmail.com>
2024-12-17 18:41:47 +0545

    Fixed the api calling in currency converter but some visual bugs remains

--- customhook/src/hooks/useCurrencyinfo.js
+++ customhook/src/hooks/useCurrencyinfo.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency){
     const[data, setData] = useState({})
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/${currency}.json`)
-        .then((res) => res.json)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency, data])
